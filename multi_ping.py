3f475892b82c2f5cd4a09b9f9c3d1b4df0bc8203
IELOCC <iel.datacollection@gmail.com>
2017-10-06 14:45:18 -0500

    Updated multiping to incorporate scanning and ping features

--- multi_ping.py
+++ multi_ping.py
@@ -9,20 +9,34 @@ import subprocess
 update = True
 Macs = []
 
-def bluetooth(address,delay=10):#Treading: Pass Delay, Mac
-    final = 0
-    command = 'sudo timeout '+str(delay)+' l2ping -c 1 '+str(address)
-    try:
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = process.communicate()
-        if "0% loss" in out: final = 1
-    except (KeyboardInterrupt, SystemExit):
-        raise
-    except:
-        print("Error with peripheral detection")
-        pass
-    #self.new_sense += (['Bluetooth',final],) #updates the tuple state
-    return final
+def ping(ping_macs,delay=10):#Treading: Pass Delay, Mac
+    output = []
+    for i in ping_macs:#only accepts lists
+        final = 0#sets our default off state
+        command = 'sudo timeout '+str(delay)+' l2ping -c 1 '+str(i)#Here's the command of interest for the night
+        try:
+            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = process.communicate()
+            if "0% loss" in out: final = 1
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            print("Error with peripheral detection")
+            pass
+        output.append(final)
+    return output
+
+def lescan(tracking, timer=10):
+    #tracking = ['E2:7C:2D:4C:E2:B4','F6:97:2C:20:BE:E4']
+    devices = subprocess.Popen('sudo timeout 'str(timer)+' lescan', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    our,err = devices.communicate()
+    output = []
+    for i in tracking:
+        if i in full_return:
+            output.append(1)
+        else:
+            output.append(0)
+    return output
 
 def email(self,file_n,name):
     try:
@@ -80,16 +94,15 @@ def clean():
     off = subprocess.Popen('sudo hciconfig hci0 reset',stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
     out,err = off.communicate()
 
-def multi(devices):
-    data = []
-    data.append(datetime.datetime.now())
-    for i in devices:
-        data.append(bluetooth(i)) #This stores the values as a tuple in a consistent manner (blue_mac, occ)
+def multi(scan_macs,ping_macs):
+    data = [datetime.datetime.now()]    #Sets the time in the first column
+    scan_out = lescan(scan_macs)        #Brings us information about the scanned devices
+    ping_out = ping(ping_macs)          #Brings information about the pingged devices
     update = False
-    return data
+    return data+scan_out+ping_out
 
 def save(dest,append_write,data):
-    with open(dest,append_write) as f:
+    with open(dest,append_write) as f:  #Writes data. Yaddah yaddah.
         writer = csv.writer(f)
         writer.writerow(data)
 
@@ -97,27 +110,35 @@ def updater():
     update = True
 
 if __name__=="__main__":
-    script_dir = os.path.dirname(os.path.abspath(__file__))
+    script_dir = os.path.dirname(os.path.abspath(__file__)) #This is our current working path
     dest_dir = os.path.join(script_dir,'Multiping_Data') #Builds a path to the file we'd like to work with
-    print(dest_dir)
+    file_name = os.path.join(dest_dir,str(datetime.datetime.now().date())+'.csv') #Builds our file name
     try:
         os.makedirs(dest_dir) #Makes a directory for saving the data
     except OSError:
-        pass
-    #June's fitbit, Thomas iPhone, June's iPhone, Zoltan's android
-    Macs = ['E5:7C:2D:4C:E2:B4','4C:57:CA:78:44:C8','1C:5C:F2:79:35:56']#,'10:30:47:34:83:A0']
-    file_name = os.path.join(dest_dir,str(datetime.datetime.now().date())+'.csv') #This puts a datestamp on the file name
+        pass #If it can't make it then it must already exist
+
+    #Thomas iPhone, June's iPhone, Zoltan's android
+    ping_macs = ['4C:57:CA:78:44:C8','1C:5C:F2:79:35:56']#,'10:30:47:34:83:A0']
+    #June's fitbit
+    scan_macs = ['E5:7C:2D:4C:E2:B4']
+
     to_email = ['juneyoungpark@utexas.edu','HagenFritz@utexas.edu','trdougherty@utexas.edu'] #Sends the data to these emails
 
     if os.path.exists(file_name):
         append_write = 'a' # append if already exists
     else:
         append_write = 'w' # make a new file if not
-    header = ['Time'];
-    for i in Macs:
-        header.append(i);
-    save(file_name,append_write,header); #This puts headers on the columns we're working with
 
+    header = ['Time']; #Builds the first column of our new file
+    #This builds our header
+    for i in scan_macs:
+        header.append(i);
+    for i in ping_macs:
+        header.append(i)
+
+    save(file_name,append_write,header); #This puts headers on the columns we're working with
+    print(header) #This shows the current header
     clean(); #This cleans the bluetooth channel
     schedule.every(1).minute.do(updater) #Makes a function that runs every minute
     while True:
@@ -128,7 +149,7 @@ if __name__=="__main__":
                 time.sleep(3)
             sys.exit()
         if update:
-            thing = multi(Macs)
-            print(thing)
+            data = multi(Macs)
+            print(data)
             save(file_name,append_write,thing);
 
